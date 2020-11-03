c6c4e5da9fd20fb2bbf4edf471333251a14a7c0c
HeenaSisodia <44946033+HeenaSisodia@users.noreply.github.com>
2020-11-03 11:27:42 +0530

    Activity7

---
+++ SQL/Activity7.sql
@@ -0,0 +1,5 @@
+Select sum(purchase_amount)as "total" from orders;
+Select avg(purchase_amout) as "average" from orders;
+Select max(purchase_amount) as "maximum" from orders;
+Select min(purchase_amount) as "minimum" from orders;
+Select count(distinct salesman_id) as "Count" from orders;
